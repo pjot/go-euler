f74dd0a537b530694cec40eb5c3e64905722c5d2
Peter Bergström <peter.bergstrom@trioptima.com>
2019-12-03 19:54:17 +0100

    optimized p64

--- p64/p64.py
+++ p64/p64.py
@@ -6,32 +6,20 @@ def continued_fraction(s):
     m = 0
     d = 1
     a = int(math.floor(math.sqrt(s)))
-    limit = int(math.log(s) * math.sqrt(s)) + 4
-    for i in range(limit):
+    while True:
         m = d * a - m
         d = (s - m * m) / d
         if d == 0:
-            return e
+            return 0
         a = int(math.floor((math.sqrt(s) + m) / d))
-        e.append((a, m, d))
-    return e
-
-def find_period(s):
-    p = 1
-    while p <= (len(s) / 2):
-        period = True
-        for i in range(p):
-            if s[i] != s[i + p]:
-                period = False
-        if period:
-            return p
-        p += 1
-    return 0
+        v = (m, d)
+        if v in e:
+            return len(e) - e.index(v)
+        e.append((m, d))
 
 odd_periods = 0
 for i in range(2, 10001):
-    expansion = continued_fraction(i)
-    period = find_period(expansion)
+    period = continued_fraction(i)
     if period > 0 and period % 2 == 1:
         odd_periods += 1
 
